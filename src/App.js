545f587e6b2baf367acc880168664859e69a9c5d
jandrocamus <arjuna1@protonmail.com>
2019-10-20 14:00:04 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AccordionExampleStyled from './Accordion/Accordion';
+
+function App() {
+  return (
+    <div className="App">
+      <AccordionExampleStyled/>
+    </div>
+  );
+}
+
+export default App;
