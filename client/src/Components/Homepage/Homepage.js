89f85bf846f7cd5505c0db5ed590b36212d873fd
jandrocamus <arjuna1@protonmail.com>
2019-10-22 15:53:32 +0200

    New folder structure for backend

---
+++ client/src/Components/Homepage/Homepage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import SearchBar from './SearchBar.js'
+import MenuButton from './MenuButton';
+
+const Homepage = () => {
+    return (
+        <div className = "homepage">
+            <div className = "topbar">
+                <div className = "ui grid">
+                    <div className = "four wide column"> 
+                    </div>
+                        <div className = "eight wide column">
+                            <div className = "logo-parent">
+                                <img src = {logo} alt = "logo" className = "logo" />
+                            </div>
+                        </div>
+                    <div className = "four wide column">
+                        <SearchBar />
+                        <MenuButton />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Homepage;
