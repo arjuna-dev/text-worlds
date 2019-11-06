488665a3246421335235c827e62484fa33e0b9d3
jandrocamus <arjuna1@protonmail.com>
2019-11-06 18:02:29 +0100

    Minimal comment delition

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,27 +3,6 @@ const User = require('../models/user');
 const {registerValidation} = require('../validation')
 const Joi = require('@hapi/joi')
 
-// const schema = Joi.object({
-//     name: Joi.string()
-//         .alphanum()
-//         .min(3)
-//         .max(30)
-//         .required(),
-
-//     password: Joi.string()
-//         .required()
-//         .pattern(/^[a-zA-Z0-9]{3,30}$/),
-
-//     repeat_password: Joi.ref('password'),
-//     // access_token: [
-//     //     Joi.string(),
-//     //     Joi.number()
-//     // ],
-//     email: Joi.string()
-//         .required()
-//         .email({ minDomainSegments: 2})
-// });
-
 
 router.post('/register',async (req, res) => {
 
