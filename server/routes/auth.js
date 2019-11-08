e4b51657cb86012073618a52e96abbb7b484a196
jandrocamus <arjuna1@protonmail.com>
2019-11-08 15:01:10 +0100

    Hasing password in working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,23 +1,30 @@
 const router = require('express').Router();
 const User = require('../models/user');
-const {registerValidation, loginValidation} = require('../validation')
+const {registerValidation, loginValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/register',async (req, res) => {
-
+    //Check for errors
     const regValidation = registerValidation(req.body)
-
     if (regValidation[0]) return res.status(400).send(regValidation[0].details[0].message)
 
+    //Check db for existing email
     const emailExists = await User.findOne({email: req.body.email})
     if(emailExists) return res.status(400).send('Email already exists')
 
-    res.send(regValidation[1]);
+    //Send for debugging in postman
+    // res.send(regValidation[1]);
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //Create user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     });
     try {
         const savedUser = await user.save();
