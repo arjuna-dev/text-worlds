5f3f1ebab0db5d41e7aa518ad603112dcf7a2c83
jandrocamus <arjuna1@protonmail.com>
2019-11-09 19:10:10 +0100

    Simple

---
+++ server/routes/auth.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const User = require('../models/user');
+const {registerValidation, loginValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+router.post('/register',async (req, res) => {
+    //Check for errors
+    const regValidation = registerValidation(req.body)
+    if (regValidation[0]) return res.status(400).send(regValidation[0].details[0].message)
+
+    //Check db for existing email
+    const emailExists = await User.findOne({email: req.body.email})
+    if(emailExists) return res.status(400).send('Email already exists')
+
+    //Send for debugging in postman
+    // res.send(regValidation[1]);
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+});
+
+router.post('/login',async (req, res) => {
+    //Check for validation errors
+    const lgnValidation = loginValidation(req.body)
+    if (lgnValidation[0]) return res.status(400).send(lgnValidation[0].details[0].message)
+
+    //Check db for existing email
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).send('This email is not registered 🤔')
+
+    //Validate password
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(400).send('Invalid password')
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
+
+    res.header('auth-token', token).send(token)
+
+    res.send('Logged in')
+});
+
+module.exports = router;
---
+++ server/validation.js
@@ -0,0 +1,47 @@
+const Joi = require('@hapi/joi')
+
+
+const registerValidation = data =>{
+    const schema = Joi.object({
+        name: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required(),
+    
+        password: Joi.string()
+            .required()
+            .pattern(/^[a-zA-Z0-9]{3,30}$/),
+    
+        repeat_password: Joi.ref('password'),
+        // access_token: [
+        //     Joi.string(),
+        //     Joi.number()
+        // ],
+        email: Joi.string()
+            .required()
+            .email({ minDomainSegments: 2})
+        });
+
+    const { error, value } = schema.validate(data);
+
+    return [error, value]
+}
+
+const loginValidation = data =>{
+    const schema = Joi.object({
+        email: Joi.string()
+        .required()
+        .email({ minDomainSegments: 2}),
+        password: Joi.string()
+            .required()
+            .pattern(/^[a-zA-Z0-9]{3,30}$/),
+        });
+
+    const { error, value } = schema.validate(data);
+
+    return [error, value]
+}
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
