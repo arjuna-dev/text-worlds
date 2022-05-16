8f52f8866db42b242e33a332c54427d7c87a0b2f
amanjaiswal03 <aman.jaiswal@code.berlin>
2022-05-16 21:06:27 +0200

    added describe to test

--- client/src/Components/World-homepage/__tests__/World.test.js
+++ client/src/Components/World-homepage/__tests__/World.test.js
@@ -7,61 +7,63 @@ import { getWorldQuery } from "../../../queries/queries";
 
 import World from "../World";
 
-it("should render world detail page without crashing", async () => {
-  const worldDetailMock = [
-    {
-      request: {
-        query: getWorldQuery,
-      },
-      result: {
-        data: {
-          world: {
-            _id: "1",
-            name: "Test",
-            description: "Testing",
-            user: {
-              name: "testUser",
-              __typename: "User",
-            },
-            posts: [
-              {
-                _id: "1",
-                title: "test",
-                text: "testing",
-                type: "Post",
-                character: {
-                  name: "King",
+describe("World detail page", () => {
+  it("should render world detail page without crashing", async () => {
+    const worldDetailMock = [
+      {
+        request: {
+          query: getWorldQuery,
+        },
+        result: {
+          data: {
+            world: {
+              _id: "1",
+              name: "Test",
+              description: "Testing",
+              user: {
+                name: "testUser",
+                __typename: "User",
+              },
+              posts: [
+                {
+                  _id: "1",
+                  title: "test",
+                  text: "testing",
+                  type: "Post",
+                  character: {
+                    name: "King",
+                  },
+                  dateCreated: "Nov 22, 2019 2:47 PM",
+                  likes: 0,
+                  deletes: 0,
+                  likesCharsId: [],
+                  deletesCharsId: [],
                 },
-                dateCreated: "Nov 22, 2019 2:47 PM",
-                likes: 0,
-                deletes: 0,
-                likesCharsId: [],
-                deletesCharsId: [],
-              },
-            ],
-            characters: [
-              {
-                _id: "1",
-                dateCreated: "Nov 22 2019",
-                name: "Paul",
-                userId: "1",
-                story: "I am a test",
-                posts: [],
-              },
-            ],
+              ],
+              characters: [
+                {
+                  _id: "1",
+                  dateCreated: "Nov 22 2019",
+                  name: "Paul",
+                  userId: "1",
+                  story: "I am a test",
+                  posts: [],
+                },
+              ],
+            },
           },
         },
       },
-    },
-  ];
+    ];
 
-  render(
-    <MockedProvider mocks={worldDetailMock} addTypename={false}>
-      <World />
-    </MockedProvider>
-  );
+    render(
+      <MockedProvider mocks={worldDetailMock} addTypename={false}>
+        <World />
+      </MockedProvider>
+    );
 
-  const worldDetailElement = await screen.findByTestId("world-item-1");
+    const worldDetailElement = await screen.findByTestId("world-item-1");
 
-  expect(worldDetailElement).toBeInTheDocument();
+    expect(worldDetailElement).toBeInTheDocument();
+  });
 });
